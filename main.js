9d1fc301c70d41285f71883d1cd6abc2e44ea09f
Alan Enriquez <alan.enriquez2001@gmail.com>
2024-01-04 19:53:55 -0600

    Create function to delete and resize grid, and added resize button event listener

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const grid = document.querySelector(".grid");
+const resizeButton = document.querySelector("button.resize-button");
+
+resizeButton.addEventListener("click", () => {
+    let gridSize = parseInt(prompt("Choose a grid size (0 < size <= 100)"));
+    createNewGrid(gridSize);
+})
 
 for (let i = 0; i < 4; i++) {
     const row = document.createElement("div");
@@ -15,5 +21,26 @@ for (row of document.getElementsByClassName("gridRow")) {
     }
 }
 
+function delete_grid() {
+    grid.innerHTML = "";
+}
 
+function createNewGrid(gridSize) {
+    delete_grid();
 
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.className = "gridRow";
+        grid.appendChild(row);
+    }
+
+    for (row of document.getElementsByClassName("gridRow")) {
+        for (let i = 0; i < gridSize; i++) {
+            const cell = document.createElement("div");
+            cell.className = "gridCell";
+            cell.addEventListener("mouseover", () => {cell.style["backgroundColor"] = "gray";})
+            row.appendChild(cell);
+        }
+    }
+
+}
